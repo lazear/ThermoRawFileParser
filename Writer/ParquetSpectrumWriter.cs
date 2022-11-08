f090beb860a1cd1a5a91f0600b31bf3c7e51f5e7
Michael Lazear <michaellazear92@gmail.com>
2022-11-08 15:50:59 -0800

    Write Parquet files in long format, remove extraneous fields

--- Writer/ParquetSpectrumWriter.cs
+++ Writer/ParquetSpectrumWriter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -11,22 +13,29 @@ using ThermoFisher.CommonCore.Data.Business;
 using ThermoFisher.CommonCore.Data.FilterEnums;
 using ThermoFisher.CommonCore.Data.Interfaces;
 using ThermoRawFileParser.Writer.MzML;
+using System.Diagnostics;
 
 namespace ThermoRawFileParser.Writer
 {
+
+    struct PScan
+    {
+        public int scan;
+        public int level;
+        public float rt;
+        public float? precursor_mz;
+        public int? precursor_charge;
+        public float mz;
+        public float intensity;
+        public int? charge;
+        public float? noise;
+    }
+
     public class ParquetSpectrumWriter : SpectrumWriter
     {
         private static readonly ILog Log =
             LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        private IRawDataPlus _rawFile;
-
-        // Dictionary to keep track of the different mass analyzers (key: Thermo MassAnalyzerType; value: the reference string)       
-        private readonly Dictionary<MassAnalyzerType, string> _massAnalyzers =
-            new Dictionary<MassAnalyzerType, string>();
-
-        private readonly Dictionary<IonizationModeType, CVParamType> _ionizationTypes =
-            new Dictionary<IonizationModeType, CVParamType>();
 
         public ParquetSpectrumWriter(ParseInput parseInput) : base(parseInput)
         {
@@ -34,22 +43,21 @@ namespace ThermoRawFileParser.Writer
 
         public override void Write(IRawDataPlus rawFile, int firstScanNumber, int lastScanNumber)
         {
-            _rawFile = rawFile;
-            List<PScan> pScans = new List<PScan>();
-            WritePScans(ParseInput.OutputDirectory, rawFile.FileName, rawFile, pScans);
+
+            var scans = Extract(rawFile);
+            WriteScans(scans, ParseInput.OutputDirectory, rawFile.FileName);
         }
 
-        private static void WritePScans(string outputDirectory, string fileName,
-            IRawDataPlus raw,
-            List<PScan> scans)
+        private static List<PScan> Extract(IRawDataPlus raw)
         {
+            var scans = new List<PScan>();
             var enumerator = raw.GetFilteredScanEnumerator(" ");
 
-            foreach (var scanNumber in enumerator
-            ) // note in my tests serial is faster than Parallel.Foreach() (this involves disk access, so it makes sense)
+            foreach (var scanNumber in enumerator)
             {
+
                 //trailer information is extracted via index
-                var trailers = raw.GetTrailerExtraValues(scanNumber);
+                //var trailers = raw.GetTrailerExtraValues(scanNumber);
                 var trailerLabels = raw.GetTrailerExtraInformation(scanNumber);
                 object chargeState = 0;
                 for (int i = 0; i < trailerLabels.Labels.Length; i++)
@@ -62,6 +70,7 @@ namespace ThermoRawFileParser.Writer
                 }
 
                 var scanFilter = raw.GetFilterForScanNumber(scanNumber);
+                int msLevel = (int)scanFilter.MSOrder;
                 var scanStats = raw.GetScanStatsForScanNumber(scanNumber);
 
                 CentroidStream centroidStream = new CentroidStream();
@@ -80,167 +89,98 @@ namespace ThermoRawFileParser.Writer
                     centroidStream.Intensities = scanData.Intensities;
                 }
 
-                var msOrder = raw.GetScanEventForScanNumber(scanNumber).MSOrder;
-
-                if (msOrder == MSOrderType.Ms)
+                if (msLevel == 1)
                 {
-                    var pscan = GetPScan(scanStats, centroidStream, fileName, Convert.ToInt32(chargeState));
-                    scans.Add(pscan);
+                    foreach (var scan in Melt(scanStats, centroidStream, msLevel))
+                    {
+                        scans.Add(scan);
+                    }
                 }
-
-                if (msOrder == MSOrderType.Ms2)
+                else if (msLevel > 1)
                 {
-                    var precursorMz = raw.GetScanEventForScanNumber(scanNumber).GetReaction(0).PrecursorMass;
-                    var pscan = GetPScan(scanStats, centroidStream, fileName, precursorMz,
-                        Convert.ToInt32(chargeState));
-                    scans.Add(pscan);
+                    var precursorMz = (float) raw.GetScanEventForScanNumber(scanNumber).GetReaction(0).PrecursorMass;
+                    foreach (var scan in Melt(scanStats, centroidStream, msLevel, precursorMz,
+                        Convert.ToInt32(chargeState)))
+                    {
+                        scans.Add(scan);
+                    }
                 }
-
-                var t = raw.GetTrailerExtraValues(scanNumber);
             }
-
-            WriteScans(outputDirectory, scans, fileName);
+            return scans;
         }
 
-        private static PScan GetPScan(ScanStatistics scanStats, CentroidStream centroidStream,
-            string fileName, double? precursorMz = null, int? precursorCharge = null)
+        // Convert scan data to long format
+        private static List<PScan> Melt(ScanStatistics scanStats, CentroidStream centroidStream,
+            int level, float? precursorMz = null, int? precursorCharge = null)
         {
-            var scan = new PScan
+            var scans = new List<PScan>();
+            for (int i = 0; i < centroidStream.Masses.Length; i++)
             {
-                FileName = fileName,
-                BasePeakMass = scanStats.BasePeakMass,
-                ScanType = scanStats.ScanType,
-                BasePeakIntensity = scanStats.BasePeakIntensity,
-                PacketType = scanStats.PacketType,
-                ScanNumber = scanStats.ScanNumber,
-                RetentionTime = scanStats.StartTime,
-                Masses = centroidStream.Masses.ToArray(),
-                Intensities = centroidStream.Intensities.ToArray(),
-                LowMass = scanStats.LowMass,
-                HighMass = scanStats.HighMass,
-                TIC = scanStats.TIC,
-                FileId = fileName,
-                PrecursorMz = precursorMz,
-                PrecursorCharge = precursorCharge,
-                MsOrder = 1
-            };
-            return scan;
+                PScan s;
+                s.scan = scanStats.ScanNumber;
+                s.level = level;
+                s.rt = (float) scanStats.StartTime;
+                s.precursor_mz = precursorMz;
+                s.precursor_charge =  precursorCharge;
+                s.intensity = (float) centroidStream.Intensities[i];
+                s.mz = (float)centroidStream.Masses[i];
+                s.charge = null;
+                s.noise = null;
+
+                if (centroidStream.Charges.Length > i)
+                {
+                    s.charge = Convert.ToInt32(centroidStream.Charges[i]);
+                }
+                if (centroidStream.Noises.Length > i)
+                {
+                    s.noise = (float)centroidStream.Noises[i];
+                }
+                scans.Add(s);
+            }
+            return scans;
         }
 
-        public static void WriteScans(string outputDirectory, List<PScan> scans, string sourceRawFileName)
+        private static void WriteScans(IEnumerable<PScan> scans, string outputDirectory, string sourceRawFileName)
         {
             var output = outputDirectory + "//" + Path.GetFileNameWithoutExtension(sourceRawFileName);
 
-            var ds = new DataSet(new DataField<double>("BasePeakIntensity"),
-                new DataField<double>("BasePeakMass"),
-                new DataField<double[]>("Baselines"),
-                new DataField<double[]>("Charges"),
-                new DataField<string>("FileId"),
-                new DataField<string>("FileName"),
-                new DataField<double>("HighMass"),
-                new DataField<double[]>("Intensities"),
-                new DataField<double>("LowMass"),
-                new DataField<double[]>("Masses"),
-                new DataField<int>("MsOrder"),
-                new DataField<double[]>("Noises"),
-                new DataField<int>("PacketType"),
-                new DataField<int?>("PrecursorCharge"),
-                new DataField<double?>("PrecursorMass"),
-                new DataField<double[]>("Resolutions"),
-                new DataField<double>("RetentionTime"),
-                new DataField<int>("ScanNumber"),
-                new DataField<string>("ScanType"),
-                new DataField<double>("TIC"));
+            var ds = new DataSet(
+                new DataField<string>("file"),
+                new DataField<int>("scan"),
+                new DataField<int>("level"),
+                new DataField<float>("rt"),
+                new DataField<float?>("precursor_mz"),
+                new DataField<int?>("precursor_charge"),
+                new DataField<float>("mz"),
+                new DataField<float>("intensity"),
+                new DataField<int?>("charge"),
+                new DataField<float?>("noise")
+             );
 
-            foreach (var scan in scans)
+            foreach ( var s in scans )
             {
-                //we can't store null values?
-                double[] dummyVal = new double[1];
-                if (scan.Noises == null)
-                {
-                    scan.Noises = dummyVal;
-                }
-
-                if (scan.Charges == null)
-                {
-                    scan.Charges = dummyVal;
-                }
-
-                if (scan.Baselines == null)
-                {
-                    scan.Baselines = dummyVal;
-                }
-
-                if (scan.Resolutions == null)
-                {
-                    scan.Resolutions = dummyVal;
-                }
-
-                if (scan.PrecursorMz == null)
-                {
-                    scan.PrecursorMz = 0;
-                    scan.PrecursorCharge = 0;
-                }
-
-                ds.Add(scan.BasePeakIntensity,
-                    scan.BasePeakMass,
-                    scan.Baselines,
-                    scan.Charges,
-                    scan.FileId,
-                    scan.FileName,
-                    scan.HighMass,
-                    scan.Intensities,
-                    scan.LowMass,
-                    scan.Masses,
-                    scan.MsOrder,
-                    scan.Noises,
-                    scan.PacketType,
-                    scan.PrecursorCharge,
-                    scan.PrecursorMz,
-                    scan.Resolutions,
-                    scan.RetentionTime,
-                    scan.ScanNumber,
-                    scan.ScanType,
-                    scan.TIC);
+                ds.Add(
+                    sourceRawFileName,
+                    s.scan,
+                    s.level,
+                    s.rt,
+                    s.precursor_mz,
+                    s.precursor_charge,
+                    s.mz,
+                    s.intensity,
+                    s.charge,
+                    s.noise
+                    );
             }
 
-            using (Stream fileStream = File.OpenWrite(output + ".parquet"))
+
+            using (Stream fileStream = File.Open(output + ".parquet", FileMode.Create))
             {
                 using (var writer = new ParquetWriter(fileStream))
                 {
-                    writer.Write(ds);
+                    writer.Write(ds, CompressionMethod.Gzip);
                 }
             }
         }
-    }
-
-    /// PSCan meaing Parsec Scan (because Commoncore has a Scan class)
-    /// </summary>
-    public class PScan
-    {
-        /// <summary>
-        /// Unique ID per file (foreign key in data store)
-        /// </summary>
-        public string FileId { get; set; }
-
-        public string FileName { get; set; }
-        public double BasePeakIntensity { get; set; }
-        public double BasePeakMass { get; set; }
-        public double[] Baselines { get; set; }
-        public double[] Charges { get; set; }
-        public double HighMass { get; set; }
-        public double[] Intensities { get; set; }
-        public double LowMass { get; set; }
-        public double[] Masses { get; set; }
-        public double[] Noises { get; set; }
-        public int PacketType { get; set; } // : 20,
-        public double RetentionTime { get; set; }
-        public double[] Resolutions { get; set; }
-        public int ScanNumber { get; set; }
-        public string ScanType { get; set; } // : FTMS + c ESI d Full ms2 335.9267@hcd30.00 [130.0000-346.0000],
-        public double TIC { get; set; }
-        public int MsOrder { get; set; }
-        public double? PrecursorMz { get; set; }
-        public int? PrecursorCharge { get; set; }
     }
 }
